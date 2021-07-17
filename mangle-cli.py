cdf8eddb1fe6cb33c9c2d2534a25f514d4a6e9b0
clach04 <clach04@gmail.com>
2021-07-16 21:13:41 -0700

    pep8 formatted cli from black

--- mangle-cli.py
+++ mangle-cli.py
@@ -27,34 +27,34 @@ from mangle.image import ImageFlags
 
 
 class FakeBook:
-	device = 'Kindle 2/3/Touch'  # See mangle/image.py KindleData.Profiles
-	outputFormat = 'CBZ only'
-	title = 'Unknown'
-	imageFlags = ImageFlags.Orient | ImageFlags.Resize | ImageFlags.Quantize
+    device = "Kindle 2/3/Touch"  # See mangle/image.py KindleData.Profiles
+    outputFormat = "CBZ only"
+    title = "Unknown"
+    imageFlags = ImageFlags.Orient | ImageFlags.Resize | ImageFlags.Quantize
 
 
 try:
-	opts, args = getopt.getopt(sys.argv[1:], 'd:t:o:')
+    opts, args = getopt.getopt(sys.argv[1:], "d:t:o:")
 except getopt.GetoptError as err:
-	print(str(err))
-	sys.exit(2)
+    print(str(err))
+    sys.exit(2)
 
-directory = '.'
+directory = "."
 
 book = FakeBook()
-book.device = 'Kindle 2/3/Touch'
-#book.device = 'Kindle Paperwhite 1 & 2'
-book.outputFormat = 'CBZ only'
-book.title = 'Unknown'
+book.device = "Kindle 2/3/Touch"
+# book.device = 'Kindle Paperwhite 1 & 2'
+book.outputFormat = "CBZ only"
+book.title = "Unknown"
 
 
-for o,a in opts:
-	if o == '-d':
-		directory = a
-	elif o == '-t':
-		book.title = a
-	elif o == '-o':
-		book.outputFormat = a
+for o, a in opts:
+    if o == "-d":
+        directory = a
+    elif o == "-t":
+        book.title = a
+    elif o == "-o":
+        book.outputFormat = a
 
 
 bookPath = os.path.join(directory, book.title)
@@ -62,19 +62,20 @@ bookPath = os.path.join(directory, book.title)
 archive = mangle.cbz.Archive(bookPath)
 
 if not os.path.isdir(bookPath):
-	os.makedirs(bookPath)
+    os.makedirs(bookPath)
 
 
 for index in range(0, len(args)):
-	target = os.path.join(bookPath, '%05d.png' % index)  # FIXME preserve original; format and name?
+    target = os.path.join(
+        bookPath, "%05d.png" % index
+    )  # FIXME preserve original; format and name?
 
-	print(index, args[index], target)  # cheap display progress
-	mangle.image.convertImage(args[index], target, str(book.device), book.imageFlags)
-	archive.addFile(target);
+    print(index, args[index], target)  # cheap display progress
+    mangle.image.convertImage(args[index], target, str(book.device), book.imageFlags)
+    archive.addFile(target)
 
 
-if 'Image' not in book.outputFormat:
-	shutil.rmtree(bookPath)
+if "Image" not in book.outputFormat:
+    shutil.rmtree(bookPath)
 
 archive.close()
-
